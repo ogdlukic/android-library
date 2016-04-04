f28ca1b2d398f311a0d71d0940000a4c3816edd3
Alexey Kuznetsov <axet@me.com>
2016-04-04 16:50:54 +0300

    fix round checkbox animation

--- src/main/java/com/github/axet/androidlibrary/widgets/RoundCheckbox.java
+++ src/main/java/com/github/axet/androidlibrary/widgets/RoundCheckbox.java
@@ -1,5 +1,6 @@
 package com.github.axet.androidlibrary.widgets;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -15,6 +16,7 @@ import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.Property;
 import android.util.StateSet;
 import android.view.Gravity;
 import android.view.ViewGroup;
@@ -34,33 +36,66 @@ import java.util.TreeMap;
 //
 public class RoundCheckbox extends CheckBox {
 
+    public static int SECOND_BACKGROUND = 0x22222222;
+
+    ObjectAnimator animator;
+
+    float stateAnimator;
+
+    // Original StateListDrawable not respect drawable ColorFilter on old phones.
+    //
+    // http://stackoverflow.com/questions/6018602
+    //
     public static class StateDrawable extends StateListDrawable {
-        HashMap<Drawable, PorterDuffColorFilter> map = new HashMap<>();
-        HashMap<Drawable, int[]> map2 = new HashMap<>();
+        HashMap<Drawable, State> map = new HashMap<>();
+
+        public static class State {
+            PorterDuffColorFilter colorFilter;
+            int[] states;
+
+            public State(PorterDuffColorFilter c, int[] s) {
+                this.colorFilter = c;
+                this.states = s;
+            }
+        }
 
         public StateDrawable() {
         }
 
         public void addState(int[] stateSet, Drawable drawable, PorterDuffColorFilter p) {
             super.addState(stateSet, drawable);
-            map.put(drawable, p);
-            map2.put(drawable, stateSet);
+            map.put(drawable, new State(p, stateSet));
         }
 
         @Override
         protected boolean onStateChange(int[] states) {
-            if (map != null) {
-                for (Drawable d : map.keySet()) {
-                    if (StateSet.stateSetMatches(map2.get(d), states)) {
-                        setColorFilter(map.get(d));
-                        break;
+            // 19 not working, 20 untested, 21 working
+            if (Build.VERSION.SDK_INT < 21) {
+                if (map != null) {
+                    for (Drawable d : map.keySet()) {
+                        if (StateSet.stateSetMatches(map.get(d).states, states)) {
+                            setColorFilter(map.get(d).colorFilter);
+                            break;
+                        }
                     }
                 }
             }
             return super.onStateChange(states);
         }
-
     }
+
+    Property<RoundCheckbox, Float> STATE_ANIMATOR = new Property<RoundCheckbox, Float>(Float.class, "stateAnimator") {
+        @Override
+        public Float get(RoundCheckbox object) {
+            return object.stateAnimator;
+        }
+
+        @Override
+        public void set(RoundCheckbox object, Float value) {
+            object.stateAnimator = value;
+            object.invalidate();
+        }
+    };
 
     public RoundCheckbox(Context context) {
         super(context);
@@ -80,17 +115,20 @@ public class RoundCheckbox extends CheckBox {
         create();
     }
 
-    void create() {
+    public void create() {
         ShapeDrawable checkbox_on = new ShapeDrawable(new OvalShape());
+        PorterDuffColorFilter checkbox_on_filter = new PorterDuffColorFilter(ThemeUtils.getThemeColor(getContext(), R.attr.colorAccent), PorterDuff.Mode.SRC_ATOP);
+        checkbox_on.setColorFilter(checkbox_on_filter);
         ShapeDrawable checkbox_off = new ShapeDrawable(new OvalShape());
+        PorterDuffColorFilter checkbox_off_filter = new PorterDuffColorFilter(SECOND_BACKGROUND, PorterDuff.Mode.MULTIPLY);
+        checkbox_off.setColorFilter(checkbox_off_filter);
 
         StateDrawable background = new StateDrawable();
-        background.addState(new int[]{android.R.attr.state_checked},
-                checkbox_on,
-                new PorterDuffColorFilter(ThemeUtils.getThemeColor(getContext(), R.attr.colorAccent), PorterDuff.Mode.SRC_ATOP));
-        background.addState(new int[]{-android.R.attr.state_checked},
-                checkbox_off,
-                new PorterDuffColorFilter(0x22222222, PorterDuff.Mode.MULTIPLY));
+        background.addState(new int[]{android.R.attr.state_checked}, checkbox_on, checkbox_on_filter);
+        background.addState(new int[]{-android.R.attr.state_checked}, checkbox_off, checkbox_off_filter);
+
+        background.setExitFadeDuration(500);
+        background.setEnterFadeDuration(500);
 
         if (Build.VERSION.SDK_INT >= 16)
             setBackground(background);
@@ -118,4 +156,16 @@ public class RoundCheckbox extends CheckBox {
 
         setTextColor(colors);
     }
+
+    @Override
+    public void setChecked(boolean checked) {
+        super.setChecked(checked);
+
+        animator = ObjectAnimator.ofFloat(this, STATE_ANIMATOR, 1f);
+        animator.setDuration(500);
+        if (Build.VERSION.SDK_INT >= 18)
+            animator.setAutoCancel(true);
+        animator.start();
+    }
+
 }
