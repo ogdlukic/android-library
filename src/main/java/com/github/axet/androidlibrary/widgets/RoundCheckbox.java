c0e2cae7977f862d4b22c5e5c3e1056a0786fc93
Alexey Kuznetsov <axet@me.com>
2016-06-30 08:58:23 +0300

    extact statedraws

--- src/main/java/com/github/axet/androidlibrary/widgets/RoundCheckbox.java
+++ src/main/java/com/github/axet/androidlibrary/widgets/RoundCheckbox.java
@@ -42,48 +42,6 @@ public class RoundCheckbox extends CheckBox {
 
     float stateAnimator;
 
-    // Original StateListDrawable not respect drawable ColorFilter on old phones.
-    //
-    // http://stackoverflow.com/questions/6018602
-    //
-    public static class StateDrawable extends StateListDrawable {
-        HashMap<Drawable, State> map = new HashMap<>();
-
-        public static class State {
-            PorterDuffColorFilter colorFilter;
-            int[] states;
-
-            public State(PorterDuffColorFilter c, int[] s) {
-                this.colorFilter = c;
-                this.states = s;
-            }
-        }
-
-        public StateDrawable() {
-        }
-
-        public void addState(int[] stateSet, Drawable drawable, PorterDuffColorFilter p) {
-            super.addState(stateSet, drawable);
-            map.put(drawable, new State(p, stateSet));
-        }
-
-        @Override
-        protected boolean onStateChange(int[] states) {
-            // 19 not working, 20 untested, 21 working
-            if (Build.VERSION.SDK_INT < 21) {
-                if (map != null) {
-                    for (Drawable d : map.keySet()) {
-                        if (StateSet.stateSetMatches(map.get(d).states, states)) {
-                            setColorFilter(map.get(d).colorFilter);
-                            break;
-                        }
-                    }
-                }
-            }
-            return super.onStateChange(states);
-        }
-    }
-
     Property<RoundCheckbox, Float> STATE_ANIMATOR = new Property<RoundCheckbox, Float>(Float.class, "stateAnimator") {
         @Override
         public Float get(RoundCheckbox object) {
